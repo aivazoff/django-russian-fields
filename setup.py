69ed30c849e9adbab929172a95008e87d1cd66d0
Arthur Aivazov <arthur.aivazoff@gmail.com>
2022-10-14 20:43:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
